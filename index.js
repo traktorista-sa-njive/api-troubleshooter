c2a2b72fae875ffadcb3b01175cbedf37796bb4c
Nemanja Filipovic <nemanja28filipo@gmail.com>
2019-05-23 00:14:23 +0200

    make instance for request

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
-
-
+const uuidv1 = require('uuid/v1');
+const moment = require('moment');
 
 
 
@@ -25,18 +25,35 @@ app.get('/', (req, res) => {
 
   app.get('/json', (req, res) => {
     res.redirect('/');
+
+    let job_id = 'get_json' + uuidv1();
+    let datetime = new Date();
+    datetime = moment().format('LLLL');  
+  
+  
+    let newReq = {
+      id: job_id,
+      type: 'GET-json',
+      date: datetime,
+      status: 'Created'
+    }
+
+
   });
 
   app.get('/xml', (req, res) => {
     res.redirect('/');
+
   });
 
   app.post('/json', (req, res) => {
     res.redirect('/');
+
   });
 
   app.post('/xml', (req, res) => {
     res.redirect('/');
+
   });
 
 
